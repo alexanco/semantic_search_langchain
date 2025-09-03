970990c084a7caa1f05ab7baf86245797ebfd4d5
Alexandre Costa <alexandre.costa@venturus.org.br>
2025-09-03 19:01:11 -0400

    feat: add ingest feature

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,61 @@
 import os
 from dotenv import load_dotenv
 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
+
 load_dotenv()
 
+# Configurar o caminho absoluto do PDF
 PDF_PATH = os.getenv("PDF_PATH")
 
 def ingest_pdf():
-    pass
+    
+    if not PDF_PATH:
+        raise ValueError("PDF_PATH is not set")
+    
+    docs = PyPDFLoader(str(PDF_PATH)).load()
+
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=150, add_start_index=False).split_documents(docs)
+    
+    if not splits:
+        raise SystemExit(0)
+
+    enriched = [
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
+    ]    
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model=os.getenv("GOOGLE_EMBEDDING_MODEL", "models/embedding-001").strip("'\""),
+        google_api_key=os.getenv("GOOGLE_API_KEY")
+    )
+
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
 
 
 if __name__ == "__main__":
+
+    for k in ("GOOGLE_API_KEY", "DATABASE_URL","PG_VECTOR_COLLECTION_NAME", "PDF_PATH"):
+        if not os.getenv(k):
+            raise RuntimeError(f"Environment variable {k} is not set")
+    
     ingest_pdf()
